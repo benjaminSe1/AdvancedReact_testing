08e051f2ad72498890893ee0c5ced48d0f164a64
Benjamin Séché <seche.benjamin@gmail.com>
2021-09-03 14:37:05 +0200

    commentbox test -> finish test on textarea

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -2,17 +2,37 @@ import React from "react";
 import { mount } from "enzyme";
 import CommentBox from "components/CommentBox";
 
-let wrapped;
+let wrapper;
+const testValue = "this is a test";
 
 beforeEach(() => {
-    wrapped = mount(<CommentBox />);
-});
-
-it("has a textarea and a button", () => {
-    expect(wrapped.find("button").length).toStrictEqual(1);
-    expect(wrapped.find("textarea").length).toStrictEqual(1);
+    wrapper = mount(<CommentBox />);
 });
 
 afterEach(() => {
-    wrapped.unmount();
+    wrapper.unmount();
+});
+
+it("has a textarea and a button", () => {
+    expect(wrapper.find("button").length).toStrictEqual(1);
+    expect(wrapper.find("textarea").length).toStrictEqual(1);
+});
+
+describe("the textarea", () => {
+    beforeEach(() => {
+        wrapper
+            .find("textarea")
+            .simulate("change", { target: { value: testValue } });
+        wrapper.update();
+    });
+    it("allows user to type in", () => {
+        expect(wrapper.find("textarea").prop("value")).toStrictEqual(testValue);
+    });
+
+    it("is emptied when form is submit", () => {
+        wrapper.find("form").simulate("submit");
+        wrapper.update();
+
+        expect(wrapper.find("textarea").prop("value")).toEqual("");
+    });
 });
