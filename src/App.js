f63ba8ed650606881a356ceec77a26300c4dc9bc
Benjamin Séché <seche.benjamin@gmail.com>
2021-09-03 11:50:58 +0200

    remove src

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">Hi There !</div>
-  );
-}
-
-export default App;
--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import App from './App';
-import ReactDOM from "react-dom";
-
-test('renders Hi There !', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  expect(div.innerHTML).toContain('Hi There !');
-  ReactDOM.unmountComponentAtNode(div);
-});
