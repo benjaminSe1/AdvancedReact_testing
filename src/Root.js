e0f0eb7cffaa2944cc0afe1e5a4a03f9a36e93f0
Benjamin Séché <seche.benjamin@gmail.com>
2021-09-06 11:38:14 +0200

    add librairies for http req

--- src/Root.js
+++ src/Root.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxPromise from "redux-promise";
+
 import reducers from "reducers";
 
 export default ({ children, intialState = {} }) => {
-    return (
-        <Provider store={createStore(reducers, intialState)}>
-            {children}
-        </Provider>
+    const store = createStore(
+        reducers,
+        intialState,
+        applyMiddleware(reduxPromise)
     );
+    return <Provider store={store}>{children}</Provider>;
 };
