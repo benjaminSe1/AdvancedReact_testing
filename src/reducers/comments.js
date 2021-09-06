07fa676e8da0d78a72895145955c45e225ca45a9
Benjamin Séché <seche.benjamin@gmail.com>
2021-09-06 10:11:22 +0200

    init actions + save_comment

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,5 +1,9 @@
+import { SAVE_COMMENT } from "actions/types";
+
 const commentsReducers = (state = [], action) => {
     switch (action.type) {
+        case SAVE_COMMENT:
+            return [...state, action.payload];
         default:
             return state;
     }
