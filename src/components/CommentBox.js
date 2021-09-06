184ce7599fb5b917bcc4cba0e46c81f727f3152d
Benjamin Séché <seche.benjamin@gmail.com>
2021-09-06 10:11:48 +0200

    use save action onSubmit

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "actions";
+
 class CommentBox extends Component {
     state = { comment: "" };
 
@@ -8,6 +11,7 @@ class CommentBox extends Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
+        this.props.saveComment(this.state.comment);
         this.setState({ comment: "" });
     };
 
@@ -27,4 +31,4 @@ class CommentBox extends Component {
     }
 }
 
-export default CommentBox;
+export default connect(null, actions)(CommentBox);
