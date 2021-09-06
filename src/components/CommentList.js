543c45faa7866e65f52629acbfa242cceda71f14
Benjamin Séché <seche.benjamin@gmail.com>
2021-09-06 11:07:43 +0200

    CommentList -> use redux

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default () => {
-    return <div>Comment list</div>
+class CommentList extends Component {
+    renderComments() {
+        return this.props.comments.map((comment) => {
+            return <li key={comment}>{comment}</li>;
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>{this.renderComments()}</ul>
+            </div>
+        );
+    }
 }
+
+function mapStatetoProps(state) {
+    return { comments: state.comments };
+}
+
+export default connect(mapStatetoProps)(CommentList);
